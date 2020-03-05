8771bb53d1a82a86e6c20ddcfa90fabb7430220f
Matthew Schwartz <mattschwartz310@gmail.com>
2020-03-05 15:59:06 -0800

    prevent apm from starting multiple times

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const apm = require('elastic-apm-node');
 
-apm.start({
-  active: true,
-  secretToken: process.env.APM_SECRET,
-  serverUrl: process.env.APM_SERVER,
-  environment: process.env.ENVIRONMENT,
-  serviceName: process.env.SERVICE_NAME,
-})
+if (!apm.isStarted()) {
+  apm.start({
+    active: true,
+    secretToken: process.env.APM_SECRET,
+    serverUrl: process.env.APM_SERVER,
+    environment: process.env.ENVIRONMENT,
+    serviceName: process.env.SERVICE_NAME,
+  })
+}
