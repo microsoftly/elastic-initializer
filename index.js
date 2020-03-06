ed4f2b00170492f3e3f7c081ae5a40a5384094e3
Matthew Schwartz <mattschwartz310@gmail.com>
2020-03-05 16:35:35 -0800

    spitballing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const apm = require('elastic-apm-node');
 
 if (!apm.isStarted()) {
+  console.log("ELASTIC-INITIALIZER: starting apm")
   apm.start({
     active: true,
     secretToken: process.env.APM_SECRET,
@@ -8,4 +9,6 @@ if (!apm.isStarted()) {
     environment: process.env.ENVIRONMENT,
     serviceName: process.env.SERVICE_NAME,
   })
+} else {
+  console.log("ELASTIC-INITIALIZER: apm already started")
 }
